65d7f03dd087556777d532eee6a402617f6465b3
Josh Rolstad <jrolstad@gmail.com>
2024-02-17 08:09:17 -0800

    Adding options for output

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ mvn dependency:tree -DoutputType=dot -DoutputFile=dependencies.dot -Dverbose=tru
 
 To run the cli
 ```shell
-go run main.go --path ..\..\data\ --filePattern *.dot
+go run main.go --path ..\..\data\ --filePattern *.dot --includedParents com.oracle --excludedDependencies com.oracle,javax. --mode notreferenced
 ```
--- internal/orchestration/dependency_analysis.go
+++ internal/orchestration/dependency_analysis.go
@@ -3,6 +3,7 @@ package orchestration
 import (
 	"github.com/jrolstad/dependency-analyzer/internal/models"
 	"github.com/jrolstad/dependency-analyzer/internal/services"
+	"slices"
 	"strings"
 )
 
@@ -28,18 +29,19 @@ func GetDependencies(path string, filePattern string, fileService services.FileS
 	return parsedDependencies, allParsedDependencies, nil
 }
 
-func IdentifyInScopeDependencies(dependencies []map[string]*models.DependencyNode) map[string]*models.DependencyNode {
+func IdentifyInScopeDependencies(dependencies []map[string]*models.DependencyNode,
+	includedParents []string,
+	excludedDependencies []string,
+	excludedScopes []string) map[string]*models.DependencyNode {
 	inScope := make(map[string]*models.DependencyNode)
 
 	for _, item := range dependencies {
 		for _, value := range item {
 			if value.Parents != nil {
 				for _, parent := range value.Parents {
-					if strings.HasPrefix(parent.FullName, "com.oracle") &&
-						!strings.HasPrefix(value.FullName, "com.oracle") &&
-						!strings.HasPrefix(value.FullName, "javax.") &&
-						!strings.EqualFold(value.Scope, "test") &&
-						!strings.EqualFold(value.Scope, "provided") {
+					if hasItemThatStartsWith(includedParents, parent.FullName) &&
+						!hasItemThatStartsWith(excludedDependencies, value.FullName) &&
+						!slices.Contains(excludedScopes, value.Scope) {
 						inScope[value.FullName] = value
 					}
 				}
@@ -51,8 +53,18 @@ func IdentifyInScopeDependencies(dependencies []map[string]*models.DependencyNod
 	return inScope
 }
 
-func IdentifyDependenciesNotReferencedByOthers(toAnalyze map[string]*models.DependencyNode) map[string]*models.DependencyNode {
-	toRemove := getDependenciesReferencedByOthers(toAnalyze)
+func hasItemThatStartsWith(allowed []string, value string) bool {
+	for _, item := range allowed {
+		if strings.HasPrefix(value, item) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IdentifyDependenciesNotReferencedByOthers(toAnalyze map[string]*models.DependencyNode, includedParents []string) map[string]*models.DependencyNode {
+	toRemove := getDependenciesReferencedByOthers(toAnalyze, includedParents)
 
 	results := make(map[string]*models.DependencyNode)
 	for _, item := range toAnalyze {
@@ -64,11 +76,12 @@ func IdentifyDependenciesNotReferencedByOthers(toAnalyze map[string]*models.Depe
 	return results
 }
 
-func getDependenciesReferencedByOthers(toAnalyze map[string]*models.DependencyNode) map[string]*models.DependencyNode {
+func getDependenciesReferencedByOthers(toAnalyze map[string]*models.DependencyNode,
+	includedParents []string) map[string]*models.DependencyNode {
 	toRemove := make(map[string]*models.DependencyNode)
 	for _, item := range toAnalyze {
 		for _, parent := range item.Parents {
-			if !strings.HasPrefix(parent.FullName, "com.oracle") {
+			if !hasItemThatStartsWith(includedParents, parent.FullName) {
 				toRemove[item.FullName] = item
 			}
 		}
